e704f0d4ec6e199c6e586b011d860df9f3d0c4fe
Romy Maghsoudi <romy_mag@outlook.com>
2020-04-25 19:29:30 -0400

    created the view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import EntrySerializer
+from .models import Entry
 
 # Create your views here.
+class EntryViewSet(viewsets.ModelViewSet):
+    queryset = Entry.objects.all().order_by('name')
+    serializer_class = EntrySerializer
